e64fa948c2c5cb30ddc5bb5f913ab9883e1eb359
Julian Meyn <Ic3Venom@gmail.com>
2017-09-22 23:41:55 -0700

    skeleton

---
+++ main.java
@@ -0,0 +1,32 @@
+package magic_numbers;
+
+public class main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Testing");
+    }
+
+    public static int[][] generateGrid(int len)
+    {
+        return null;
+    }
+
+    public static boolean isMagic(int[][] grid)
+    {
+        int base = 0;
+
+        for(int i = 0; i < int[0].length; i++)
+        {
+            base += len[0][i];
+        }
+
+        // Horizontal
+        
+        // Vertical
+
+        // Diagional
+
+        return true;
+    }
+}
